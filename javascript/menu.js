8e9b51a784eac3583891dc19ef64c47810ad0cb6
John-Douglas-Hynes <j.hynes85v@gmail.com>
2023-01-11 14:46:44 -0600

    Update mobile navigation tabs

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,8 +1,24 @@
 const floatingTabs = document.querySelector(".floating-tabs");
-const menuButton = document.getElementById("navTab")
+const menuButton = document.getElementById("navTab");
+const linksButton = document.getElementById("linksTab");
+const navMenu = document.querySelector(".left-box");
+const linksMenu = document.querySelector(".right-box");
 
-menuButton.addEventListener("click", pulloutMenu);
+menuButton.addEventListener("click", pulloutMenuNav);
+linksButton.addEventListener("click", pulloutMenuLinks);
 
-function pulloutMenu() {
-    floatingTabs.style.display = "none";
+function pulloutMenuNav() {
+    menuButton.style.display = "none";
+    linksButton.style.display = "none";
+    navMenu.style.width = "100%";
+    navMenu.style.margin = "0";
+    floatingTabs.appendChild(navMenu);
 };
+
+function pulloutMenuLinks() {
+    menuButton.style.display = "none";
+    linksButton.style.display = "none";
+    linksMenu.style.width = "100%";
+    linksMenu.style.margin = "0";
+    floatingTabs.appendChild(linksMenu);
+}
